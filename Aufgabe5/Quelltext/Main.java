5c76d0b16104b67189453ef33fe9a32cddbd90ff
leartpro <lenni.protte@gmx.de>
2022-10-20 19:07:39 +0200

    Datastructures and read input

--- Aufgabe5/Quelltext/Main.java
+++ Aufgabe5/Quelltext/Main.java
@@ -3,20 +3,39 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.BitSet;
 import java.util.List;
 import java.util.stream.Stream;
 
 public class Main {
-    boolean[][] graph;
+    private static final List<BitSet[]> graphs = new ArrayList<>();
 
     public static void main(String[] args) {
-        for(Path path :getPaths()) {
-            for(String line : getLines(path)) {
-                System.out.println(line);
+        for (Path path : getPaths()) {
+            List<String> lines = getLines(path);
+            int countOfNodes = Integer.parseInt(lines.get(0).split(" ")[0]);
+            //TODO: check if this variable is needed
+            int countOfArrows = Integer.parseInt(lines.get(0).split(" ")[1]);
+            //initialisation of the graph
+            BitSet[] graph = new BitSet[countOfNodes];
+            //declaration of the graph
+            for (int i = 0; i < countOfNodes; i++) {
+                graph[i] = new BitSet(countOfNodes);
             }
+            //writing input
+            for (String line : lines) {
+                int output_field, target_field;
+                output_field = Integer.parseInt(line.split(" ")[0]);
+                target_field = Integer.parseInt(line.split(" ")[1]);
+                graph[output_field].set(target_field);
+            }
+            graphs.add(graph);
         }
     }
 
+    /**
+     * @return A list of paths to all files with the .txt extension in the Aufgabe5/Eingabedateien/ directory.
+     */
     private static List<Path> getPaths() {
         List<Path> paths;
         try (Stream<Path> walk = Files.walk(Paths.get("Aufgabe5/", "Eingabedateien/"))) {
@@ -26,6 +45,11 @@ public class Main {
         }
         return paths;
     }
+
+    /**
+     * @param path The file from which the lines are read
+     * @return A list of strings representing the lines
+     */
     private static List<String> getLines(Path path) {
         List<String> data;
         try (Stream<String> lines = Files.lines(path)) {
