c1451af825e60f4a47e094a42ffdc91d0110e377
leartpro <lenni.protte@gmx.de>
2022-10-20 18:33:26 +0200

    Input Files

--- Aufgabe5/Quelltext/Main.java
+++ Aufgabe5/Quelltext/Main.java
@@ -1,5 +1,38 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Main {
+    boolean[][] graph;
+
     public static void main(String[] args) {
-        System.out.println("s");
+        for(Path path :getPaths()) {
+            for(String line : getLines(path)) {
+                System.out.println(line);
+            }
+        }
+    }
+
+    private static List<Path> getPaths() {
+        List<Path> paths;
+        try (Stream<Path> walk = Files.walk(Paths.get("Aufgabe5/", "Eingabedateien/"))) {
+            paths = walk.filter(Files::isRegularFile).filter(path -> path.getFileName().toString().endsWith(".txt")).toList();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return paths;
+    }
+    private static List<String> getLines(Path path) {
+        List<String> data;
+        try (Stream<String> lines = Files.lines(path)) {
+            data = new ArrayList<>(lines.toList());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return data;
     }
 }
