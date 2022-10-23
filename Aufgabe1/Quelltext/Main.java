e94f33000b53c169e07e932852726da1b63288f0
Tony <65500763+bit-burger@users.noreply.github.com>
2022-10-23 12:58:16 +0200

    Ez win gg wp

--- Aufgabe1/Quelltext/Main.java
+++ Aufgabe1/Quelltext/Main.java
@@ -3,6 +3,8 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 public class Main {
@@ -10,97 +12,28 @@ public class Main {
     private static final List<String>[] stoerungen = new ArrayList[6];
 
     public static void main(String[] args) {
+        String text = null;
         for (Path path : getPaths()) {
             if (path.getFileName().toString().equals("Alice_im_Wunderland.txt")) {
-                book.addAll(getLines(path));
-            } else {
-                for (String line : getLines(path)) {
-                    List<String> wordsList = new ArrayList<>();
-                    String[] words = line.split(" ");
-                    Collections.addAll(wordsList, words);
-                    stoerungen[Integer.parseInt(path.getFileName().toString().replaceAll("[^0-9]+", ""))] = wordsList;
+                text = getText(path).toLowerCase();
+            }
+        }
+        assert text != null: "text has not been found in alice_im_wunderland.txt";
+        for (Path path : getPaths()) {
+            if (!path.getFileName().toString().equals("Alice_im_Wunderland.txt")) {
+                String rawRegexStr = getText(path)
+                        .replaceAll(" ", "[ \\\\n]")
+                        .replaceAll("_", "[^.»«!?/\\\\n ]+"); //,_#
+                System.out.println(path.getFileName() + ": ");
+                Pattern regex = Pattern.compile(rawRegexStr);
+                Matcher matcher = regex.matcher(text);
+                while(matcher.find()) {
+                    System.out.println(matcher.group() + "\n");
                 }
+                System.out.println("\n");
             }
         }
-        for (int i = 0; i < stoerungen.length; i++) {
-            List<int[]> results = checkPositions(getPositions(stoerungen[i]), stoerungen[i]);
-            System.out.println("Störung Nr." + (i + 1) + ":");
-            for (int index = 0; index < stoerungen[i].size(); index++) {
-                System.out.print(stoerungen[i].get(index) + " ");
-            }
-            System.out.println();
-            for (int[] result : results) {
-                System.out.println("Zeile: " + result[0]);
-                System.out.println("'" + getText(result[0], result[1]) + "'");
-            }
-            System.out.println();
-        }
     }
-
-    private static List<int[]> getPositions(List<String> stoerung) {
-        List<int[]> positions = new ArrayList<>();
-        for (int lineNumber = 0; lineNumber < book.size(); lineNumber++) {
-            String line = book.get(lineNumber).toLowerCase();
-            int index = line.indexOf(stoerung.get(0));
-            while (index >= 0) {
-                positions.add(new int[]{lineNumber, index});
-                index = line.indexOf(stoerung.get(0), index + 1);
-            }
-        }
-        return positions;
-    }
-
-    private static List<int[]> checkPositions(List<int[]> positions, List<String> stoerung) {
-        List<int[]> rightPositions = new ArrayList<>();
-        positions.forEach(position -> {
-            StringBuilder text = new StringBuilder(book.get(position[0]).substring(position[1]).toLowerCase());
-            int counter = 1;
-             while (text.toString().split(" ").length < stoerung.size()) {
-                if (position[0] + counter >= book.size()) {
-                    return;
-                }
-                text.append(" ").append(book.get(position[0] + counter).toLowerCase()); // TODO: bei punkt oder zwei neuen Zeilen abrechen
-                counter++;
-            }
-            String[] lines = text.toString().split(" ");
-            for (int index = 0; index < lines.length; index++) {
-                lines[index] = lines[index].replaceAll("[^[a-zA-Z0-9äöüß]]", "");
-            }
-            for (int index = 1; index < stoerung.size(); index++) {
-                if (Objects.equals(stoerung.get(index), lines[index])) {
-                    if (index == stoerung.size() - 1) {
-                        rightPositions.add(position);
-                    }
-                } else if (!Objects.equals(stoerung.get(index), "_")) {
-                    break;
-                } else {
-                    if (index == stoerung.size() - 1) {
-                        rightPositions.add(position);
-                    }
-                }
-            }
-        });
-        return rightPositions;
-    }
-    private static String getText(int line, int column) {
-        StringBuilder text = new StringBuilder(book.get(line));
-        int startIndex = text.toString().replaceAll("[.!?,»«()\\[\\]]", "#").lastIndexOf("#", column);
-        int endIndex = text.toString().replaceAll("[.!?,»«()\\[\\]]", "#").indexOf("#", column);
-        int counter = 1;
-        while (startIndex < 0 || endIndex < 0) {
-            if (startIndex < 0) {
-                text.insert(0, (book.get(line - counter) + " "));
-            }
-            if (endIndex < 0) {
-                text.append(" ").append(book.get(line + counter));
-            }
-            startIndex = text.toString().replaceAll("[.!?,»«()\\[\\]]", "#").lastIndexOf("#", column);
-            endIndex = text.toString().replaceAll("[.!?,»«()\\[\\]]", "#").indexOf("#", column);
-            counter++;
-        }
-        return text.substring(startIndex + 1, endIndex + 1).trim();
-    }
-
     private static List<Path> getPaths() {
         List<Path> paths;
         try (Stream<Path> walk = Files.walk(Paths.get("Aufgabe1/", "Eingabedateien/"))) {
@@ -111,13 +44,11 @@ public class Main {
         return paths;
     }
 
-    private static List<String> getLines(Path path) {
-        List<String> data;
-        try (Stream<String> lines = Files.lines(path)) {
-            data = new ArrayList<>(lines.toList());
-        } catch (IOException e) {
+    private static String getText(Path path) {
+        try {
+            return Files.readString(path);
+        } catch(IOException e) {
             throw new RuntimeException(e);
         }
-        return data;
     }
 }
