d4cb59116562ecabdc91d492d23498352c9c3f56
Olevenbaum <88549096+Olevenbaum@users.noreply.github.com>
2022-10-23 13:16:29 +0200

    Changes by Lenni and Tony
    Changes by Lenni and Tony

--- Aufgabe1/Quelltext/Main.java
+++ Aufgabe1/Quelltext/Main.java
@@ -5,8 +5,12 @@ import java.nio.file.Paths;
 import java.util.*;
 import java.util.stream.Stream;
 
+/**
+ *
+ */
 public class Main {
     private static final List<String> book = new ArrayList<>();
+    @SuppressWarnings("unchecked")
     private static final List<String>[] stoerungen = new ArrayList[6];
 
     public static void main(String[] args) {
@@ -26,6 +30,10 @@ public class Main {
         }
     }
 
+    /**
+     * @param stoerung Die Eingabedatei, nach der das Buch durchsucht werden soll
+     * @return Eine Liste von Positionen, die mit demselben Wort beginnen wie die Eingabedatei
+     */
     private static List<int[]> getPositions(List<String> stoerung) {
         List<int[]> positions = new ArrayList<>();
         for (int lineNumber = 0; lineNumber < book.size(); lineNumber++) {
@@ -39,17 +47,26 @@ public class Main {
         return positions;
     }
 
+    /**
+     * @param positions Eine Liste von Positionen, die überprüft werden soll
+     * @param stoerung Die Eingabedatei, nach der die möglichen Positionen überprüft werden sollen
+     * @return Eine Liste von Positionen, die die Eingabedatei lösen können
+     */
     private static List<int[]> checkPositions(List<int[]> positions, List<String> stoerung) {
         List<int[]> rightPositions = new ArrayList<>();
-        positions.forEach(position -> {
+        positionLoop:
+        for (int[] position : positions) {
             StringBuilder text = new StringBuilder(book.get(position[0]).substring(position[1]).toLowerCase());
             int counter = 1;
-             while (text.toString().split(" ").length < stoerung.size()) {
+            while (text.toString().split(" ").length < stoerung.size()) {
                 if (position[0] + counter >= book.size()) {
-                    return;
+                    break positionLoop;
                 }
                 text.append(" ").append(book.get(position[0] + counter).toLowerCase());
                 counter++;
+            }
+            if (text.toString().contains("  ")) {
+                continue;
             }
             String[] lines = text.toString().split(" ");
             for (int index = 0; index < lines.length; index++) {
@@ -68,9 +85,15 @@ public class Main {
                     }
                 }
             }
-        });
+        }
         return rightPositions;
     }
+
+    /**
+     * @param line Die Zeile, in der der Text beginnt
+     * @param column Die Spalte, in der der Text beginnt
+     * @return Der (Teil-)Satz, der sich an der durch die Parameter bestimmten Stelle befindet
+     */
     private static String getText(int line, int column) {
         StringBuilder text = new StringBuilder(book.get(line));
         int startIndex = formatText(text).lastIndexOf("#", column);
@@ -90,6 +113,36 @@ public class Main {
         return text.substring(startIndex + 1, endIndex + 1).trim();
     }
 
+    /**
+     * @param text Der zu bearbeitende Text
+     * @return Der bearbeitete Text
+     */
+    private static String formatText(StringBuilder text) {
+        return text.toString().replaceAll("[.!?,»«'\"()\\[\\]]", "#");
+    }
+
+    /**
+     *
+     */
+    private static void evaluatingFiles() {
+        for (Path path : getPaths()) {
+            if (path.getFileName().toString().equals("Alice_im_Wunderland.txt")) {
+                book.addAll(getLines(path));
+            } else {
+                for (String line : getLines(path)) {
+                    List<String> wordsList = new ArrayList<>();
+                    String[] words = line.split(" ");
+                    Collections.addAll(wordsList, words);
+                    stoerungen[Integer.parseInt(path.getFileName().toString().replaceAll("[^0-9]+", ""))] = wordsList;
+                }
+            }
+        }
+    }
+
+    /**
+     * @return Eine Liste der Pfade zu allen Dateien im Verzeichnis Aufgabe1/Eingabedateien/*
+     * @throws RuntimeException wenn nicht auf den Pfad zugegriffen werden kann
+     */
     private static List<Path> getPaths() {
         List<Path> paths;
         try (Stream<Path> walk = Files.walk(Paths.get("Aufgabe1/", "Eingabedateien/"))) {
@@ -100,6 +153,11 @@ public class Main {
         return paths;
     }
 
+    /**
+     * @param path Der Pfad, unter dem die Datei gelesen werden soll
+     * @return Eine Liste der Zeilen in der Datei
+     * @throws RuntimeException wenn nicht auf die Datei zugegriffen werden kann
+     */
     private static List<String> getLines(Path path) {
         List<String> data;
         try (Stream<String> lines = Files.lines(path)) {
