82c188805bc402cc679fedda183ea3832a30f6d2
Tony <65500763+bit-burger@users.noreply.github.com>
2022-11-11 23:52:34 +0100

    remove unused code

--- Aufgabe4/Quelltext/Main.java
+++ Aufgabe4/Quelltext/Main.java
@@ -4,31 +4,14 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Main {
-    public static final String TEXT_RED = "\u001B[31m";
-    public static final String TEXT_BLACK = "\u001B[30m";
     public static final String TEXT_GREEN = "\u001B[32m";
     public static final String TEXT_BLUE = "\u001B[34m";
     public static final String TEXT_RESET = "\u001B[0m";
-    public static final String TEXT_PURPLE = "\u001B[35m";
-    public static final String TEXT_CYAN = "\u001B[36m";
-    public static final String TEXT_YELLOW = "\u001B[33m";
-    public static final String TEXT_WHITE = "\u001B[37m";
 
-//    private record Task(int entranceTime, int duration) {
-//    }
-
-    private static class Task {
-        final int entranceTime;
-        final int duration;
-
-        private Task(int entranceTime, int duration) {
-            this.entranceTime = entranceTime;
-            this.duration = duration;
-        }
+    private record Task(int entranceTime, int duration) {
     }
 
     abstract private static class TaskPriorityDelegate {
