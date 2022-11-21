666050ab2c2b3c9b794f729b521bbf44c9704213
leartpro <lenni.protte@gmx.de>
2022-11-21 22:18:24 +0100

    Cleanup

--- Aufgabe4/VergleichAusgabeDateien/vergleich.sh
+++
@@ -1,5 +0,0 @@
-cd ../..
-
-javac -d Aufgabe4/target Aufgabe4/Quelltext/Main.java;
-
-java -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath Aufgabe4/target Main | diff - Aufgabe4/vergleichausgabedateien/richtige-ausgabe-beides.txt
